9235b95722c4d2a59ebf5f56555fb0f11bb886a4
fjooner <32850191+fjooner@users.noreply.github.com>
2017-10-16 23:21:44 +0100

    223
    123312

--- app.py
+++ app.py
@@ -2,6 +2,5 @@ import json
 import urllib
 url = "http://api.openweathermap.org/data/2.5/forecast?id=3155573&APPID=0aa43b6005881a082d2282b698788d1c"
 response = urllib.urlopen(url)
-data = json.loads(response.read())
-#json_data = response.json()
-print data
+data = json.load(response)
+print (data)
